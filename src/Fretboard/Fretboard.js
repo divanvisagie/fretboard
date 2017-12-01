a2b362a1ad4ff5c6ce9c8be91097d203d0260dac
Divan Visagie <visagiedivan@gmail.com>
2017-12-01 14:27:52 +0200

    create fretboard component

---
+++ src/Fretboard/Fretboard.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './Fretboard.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="Fretboard">
+            </div>
+        );
+    }
+}
