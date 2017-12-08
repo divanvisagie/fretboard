3466812e2d61fdaae46b50020f79618787076425
Divan Visagie <visagiedivan@gmail.com>
2017-12-08 15:35:56 +0200

    well thats strict

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-import Fretboard from './Fretboard/Fretboard'
 
 class App extends Component {
   render () {
