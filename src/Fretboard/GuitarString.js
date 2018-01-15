57140e307c5a820a9d7760f5ba4ec0c0872c1d2f
Divan Visagie <visagiedivan@gmail.com>
2018-01-15 07:53:18 +0200

    styling

--- src/Fretboard/GuitarString.js
+++ src/Fretboard/GuitarString.js
@@ -11,28 +11,27 @@ import Note from '../Core/Note'
 class GuitarString extends Component {
   constructor (props) {
     super(props)
-
     const guitarString = GuitarStringModel(this.props.rootNote, this.props.frets)
     const rootNote = Note(this.props.rootNote)
     const notes = _.range(this.props.frets).map((x) => {
       const fret = parseInt(x, 10)
-      return guitarString.noteAtFret(fret).toString()
+      return guitarString.noteAtFret(fret)
     })
 
     this.notes = notes
+    this.rootNote = rootNote
   }
 
   render () {
     return (
       <div className="GuitarString">
-        Root Note: {this.props.rootNote}
-        <br/>
-        Frets: {this.props.frets}
-        {this.test}
+        <div className='note'>
+          {this.rootNote.toString()}
+        </div>
 
         {this.notes.map((noteName) =>
-          <div>
-            {noteName}
+          <div className='note'>
+            {noteName.toString()}
           </div>
         )}
       </div>
