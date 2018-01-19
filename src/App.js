d306b190d4be98fa18b563e848f34a3bc074ec28
Divan Visagie <visagiedivan@gmail.com>
2018-01-19 20:36:33 +0200

    update icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './App.css'
-
+import icon from './icon.png'
+import Icon from './Icon/Icon'
 import Fretboard from './Fretboard/Fretboard'
 
 class App extends Component {
@@ -8,9 +9,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+        
           <h1 className="App-title">Welcome to Fretboard</h1>
         </header>
-
+        <div className="App-logo">
+          <img src={icon} />
+          </div>
         <div className="App-fretboard-container">
           <Fretboard/>
         </div>
