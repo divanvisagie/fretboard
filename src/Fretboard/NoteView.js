56607dc02c83858bdfda67195c8ab073f5c24a71
Divan Visagie <visagiedivan@gmail.com>
2018-01-19 14:13:54 +0200

    better frets

--- src/Fretboard/NoteView.js
+++ src/Fretboard/NoteView.js
@@ -7,10 +7,13 @@ class NoteView extends Component {
   render () {
     return (
       <div className='NoteView'>
-        <div className='string'></div>
-        <div className='note'>
-          {this.props.note.toString()}
+        <div className='note-area'>
+          <div className='string'></div>
+          <div className='note'>
+            {this.props.note.toString()}
+          </div>
         </div>
+        <div className='fret'></div>
       </div>
     )
   }
