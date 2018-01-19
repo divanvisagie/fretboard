b1cb0da9dae85ca2a60b43c53294e11acccbc30b
Divan Visagie <visagiedivan@gmail.com>
2018-01-19 14:27:58 +0200

    Title change

--- src/App.js
+++ src/App.js
@@ -9,13 +9,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Fretboard</h1>
         </header>
-        <p className="App-intro">
-            To get started, edit <code>src/App.js</code>
-            and save to reload.
-        </p>
+      
         <p>
           <Fretboard/>
         </p>
