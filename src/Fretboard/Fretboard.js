df105760d2339654037427fc70f3fe4fcfdf1010
Divan Visagie <visagiedivan@gmail.com>
2018-01-26 14:54:35 +0200

    slightly better css

--- src/Fretboard/Fretboard.js
+++ src/Fretboard/Fretboard.js
@@ -10,12 +10,14 @@ class Fretboard extends Component {
     const frets = 24
     return (
       <div className="Fretboard">
-        <GuitarString rootNote='E' frets={frets}/>
-        <GuitarString rootNote='B' frets={frets}/>
-        <GuitarString rootNote='G' frets={frets}/>
-        <GuitarString rootNote='D' frets={frets}/>
-        <GuitarString rootNote='A' frets={frets}/>
-        <GuitarString rootNote='E' frets={frets}/>
+        <div>
+          <GuitarString rootNote='E' frets={frets}/>
+          <GuitarString rootNote='B' frets={frets}/>
+          <GuitarString rootNote='G' frets={frets}/>
+          <GuitarString rootNote='D' frets={frets}/>
+          <GuitarString rootNote='A' frets={frets}/>
+          <GuitarString rootNote='E' frets={frets}/>
+        </div>
         <MarkerBoard frets={frets}/>
       </div>
     )
