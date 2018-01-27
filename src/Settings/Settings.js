320dbeceab41f9f3102106fc3f1409e0afb7abb2
Divan Visagie <visagiedivan@gmail.com>
2018-01-27 16:15:23 +0200

    Add min and max for frets

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
 const Settings = ({handleFretsChange, frets}) =>
-  <input type="number" onChange={handleFretsChange} value={frets} />
+  <input type="number" onChange={handleFretsChange} value={frets} max="27" min="12" />
 
 export default Settings
