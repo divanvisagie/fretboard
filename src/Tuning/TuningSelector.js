05f73b3b229d8ec69262fa3ebf6d7a4268929654
Divan Visagie <visagiedivan@gmail.com>
2018-01-28 10:53:35 +0200

    Cleanup

--- src/Tuning/TuningSelector.js
+++ src/Tuning/TuningSelector.js
@@ -3,14 +3,6 @@ import React from 'react'
 import Select from 'material-ui/Select'
 import { MenuItem } from 'material-ui/Menu'
 
-// const TuningSelector = ({tuning, handleTuningChange, tuningOptions}) =>
-//   <select value={tuning.name}
-//     onChange={handleTuningChange} >
-//     {tuningOptions.map((x, key) =>
-//       <option value={x} key={key}>{x}</option>
-//     )}
-//   </select>
-
 const TuningSelector = ({tuning, handleTuningChange, tuningOptions}) =>
   <Select
     value={tuning.name}
