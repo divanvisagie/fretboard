3e1a85f4b366c8bc45d0e5b153178da59d184d2e
Divan Visagie <visagiedivan@gmail.com>
2018-02-16 13:48:58 +0200

    add scalle

---
+++ src/Core/Scale.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const scales = [
+  {
+    name    : 'Major',
+    sequence: [2, 2, 1, 2, 2, 2, 1]
+  },
+  {
+    name    : 'Natural Minor',
+    sequence: [2, 1, 2, 2, 1, 2, 2]
+  }
+]
+
+export {
+  scales
+}
