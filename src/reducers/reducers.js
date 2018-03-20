0e8f37a332177fc166da5b425c654a656ca7806e
Divan Visagie <visagiedivan@gmail.com>
2018-03-20 15:25:57 +0200

    Add reducer for key

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -18,9 +18,17 @@ function focusNote (state = '', action) {
   }
 }
 
+function key (state = '', action) {
+  if (action.type === 'SET_KEY') {
+    state = action.value
+  }
+  return state
+}
+
 export default combineReducers({
   focusNote,
   frets,
+  key,
   tuningOptions,
   tuning
 })
