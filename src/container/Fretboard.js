4162480b6114ef5970a3c9cf7e5b2650d3b6a5db
Divan Visagie <me@dvisagie.com>
2019-02-09 11:57:09 +0200

    Cleanup on Fret and Marker boards

--- src/container/Fretboard.js
+++ src/container/Fretboard.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {connect} from 'react-redux'
 
 import './Fretboard.css'
@@ -9,42 +9,29 @@ import MarkerBoard from '../presentational/MarkerBoard'
 
 import GuitarString from '../presentational/GuitarString'
 
-class Fretboard extends Component {
-    clearDisabled () {
-        return this.props.selectedNotes.length < 1
-    }
-
-    render () {
-        const {
-            tuning,
-            frets,
-            scaleNotes,
-            onNoteClick,
-            focusNote,
-            scale,
-            selectedNotes
-        } = this.props
-
-        return (
-            <div className='Fretboard noselect'>
-                <div>
-                    {reverse(tuning).map((note, i) =>
-                        <GuitarString rootNote={note}
-                            frets={frets}
-                            scale={scale}
-                            onClick={n => onNoteClick(n, i)}
-                            key={i}
-                            string={i}
-                            scaleNotes={scaleNotes}
-                            selectedNotes={selectedNotes}
-                            focusNote={focusNote} />
-                    )}
-                </div>
-                <MarkerBoard frets={frets}/>
-            </div>
-        )
-    }
-}
+const Component = ({tuning,
+    frets,
+    scaleNotes,
+    onNoteClick,
+    focusNote,
+    scale,
+    selectedNotes}) =>
+    <div className='Fretboard noselect'>
+        <div>
+            {reverse(tuning).map((note, i) =>
+                <GuitarString rootNote={note}
+                    frets={frets}
+                    scale={scale}
+                    onClick={n => onNoteClick(n, i)}
+                    key={i}
+                    string={i}
+                    scaleNotes={scaleNotes}
+                    selectedNotes={selectedNotes}
+                    focusNote={focusNote} />
+            )}
+        </div>
+        <MarkerBoard frets={frets}/>
+    </div>
 
 const mapStateToProps = (state) => {
     return {
@@ -70,9 +57,9 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-const FretboardConnector = connect(
+const Fretboard = connect(
     mapStateToProps,
     mapDispatchToProps
-)(Fretboard)
+)(Component)
 
-export default FretboardConnector
+export default Fretboard
--- src/presentational/MarkerBoard.js
+++ src/presentational/MarkerBoard.js
@@ -1,7 +1,5 @@
-import React, {Component} from 'react'
-
+import React from 'react'
 import './MarkerBoard.css'
-import PropTypes from 'prop-types'
 
 function getMarker (index) {
     if (index === 0) return ''
@@ -17,64 +15,15 @@ function getMarker (index) {
     return ''
 }
 
-class MarkerBoard extends Component {
-  state = {
-      range: []
-  }
-
-  constructor (props) {
-      super(props)
-
-      this.state.range = this.getRange(this.props.frets)
-
-      this.dots = [
-          3, 5, 7, 9, 12, 15, 17, 19, 21, 24
-      ]
-  }
-
-  getRange (frets) {
-      let range = []
-      for (let i = 1; i < frets + 1; i++) {
-          range.push(i)
-      }
-      return range
-  }
-
-  drawDot (value) {
-      if (this.dots.includes(value)) {
-          if (value % 12 === 0) {
-              return <div className="dot">{value}</div>
-          } else {
-              return <div className="dot">{value}</div>
-          }
-      } else {
-          return <div></div>
-      }
-  }
-
-  componentWillReceiveProps (nextProps) {
-      this.setState({
-          range: this.getRange(nextProps.frets)
-      })
-  }
-
-  render () {
-      return (
-          <div className="MarkerBoard">
-              <div className="nut"></div>
-
-              {this.state.range.map((x, i) =>
-                  <div className="fret" key={i}>
-                      {getMarker(i + 1)}
-                  </div>
-              )}
-          </div>
-      )
-  }
-}
-
-MarkerBoard.propTypes = {
-    frets: PropTypes.number
-}
+const MarkerBoard = ({frets}) => (
+    <div className='MarkerBoard'>
+        <div className='nut'></div>
+        {[...Array(frets)].map((_x, i) =>
+            <div className="fret" key={i}>
+                {getMarker(i + 1)}
+            </div>
+        )}
+    </div>
+)
 
 export default MarkerBoard
